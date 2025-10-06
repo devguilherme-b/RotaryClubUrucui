985a7d885fa385d576d471c8c881e6c095426a94
devguilherme-b <hitsnoplaymusic@gmail.com>
2025-10-06 16:51:07 -0300

    feat: add responsive section blog for desktop, tablets and mobile

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,18 +6,3 @@
         alert('Erro ao copiar o conteúdo: ', err);
     });
 }
-
-// TO INICIALIZE SWIPER
-
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    effect: "fade",
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
