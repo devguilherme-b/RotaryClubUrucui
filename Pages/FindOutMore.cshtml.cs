d4911d1e21f3413e69c8dd56f12939b7a6fad2f3
devguilherme-b <hitsnoplaymusic@gmail.com>
2025-10-15 20:50:12 -0300

    feat: Add Fid Out More Page responsive for mobile, tablets and desktop.
    fit: correct the color of the section topic on.
    
    refactor: refactor styling file organization into folders named after the respective pages.

---
+++ Pages/FindOutMore.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RotaryClubUrucui.Pages
+{
+    public class FindOutMoreModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
